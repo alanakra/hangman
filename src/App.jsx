1f5f81d9ada1fae088b5d3acc71b838ac2c441d4
Alan Akra <alanakra@hotmail.fr>
2024-10-01 11:32:41 +0200

    Fetch word

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,41 @@
-
+import { useEffect, useState } from "react"
 
 function App() {
+  const [word, setWord] = useState('')
+  const [loading, setLoading] = useState(true)
+  const [fetchError, setFetchError] = useState(null)
+
+  useEffect(() => {
+    async function fetchWord() {
+      try {
+        const response = await fetch("http://localhost:3333", {
+          method: 'POST'
+        }
+        )
+        const data = await response.json()
+        setLoading(false)
+        if (word === "") {
+          setWord(data.word)
+        }
+      } catch (error) {
+        console.error("Error fetching the word:", error)
+        setLoading(false)
+        setFetchError(true)
+      }
+    }
+
+    fetchWord();
+  }, [])
+
+  if (loading) return <h1>Fetching word...</h1>
+  if (fetchError) return <h1>Word not loaded:</h1>
+
   return (
     <>
-      <h1>Hangman</h1>
+      <div className="top">
+        <h1>Hangman</h1>
+        <h2 className="word">{word.toUpperCase()}</h2>
+      </div>
     </>
   )
 }
