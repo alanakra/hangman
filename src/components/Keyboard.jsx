0685ee3f941652c1dc86b811a52707fd5cc3492e
Alan Akra <alanakra@hotmail.fr>
2024-10-04 09:26:45 +0200

    One handleClickBtn event listener

--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -20,9 +20,11 @@ export default function Keyboard({rightWord, setGoodResponseList, badResponseLis
     }, [])
 
     const handleClickBtn = e => {
-        const letter = e.target.getAttribute('data-letter').toLowerCase()
-        setLetterInput(letter)
-        isLetterIncluded(letterInput)
+        const letter = e.target.getAttribute('data-letter')
+        if (letter != null) {
+            setLetterInput(letter)
+            isLetterIncluded(letterInput)
+        }
     }
 
     const isLetterIncluded = letterInput => {
@@ -42,39 +44,39 @@ export default function Keyboard({rightWord, setGoodResponseList, badResponseLis
     return(
         <>
             <h3 style={{textAlign: 'center', textTransform: 'uppercase'}}>{letterInput}</h3>
-            <div className="keyboard-cont">
+            <div className="keyboard-cont" onClick={handleClickBtn}>
                 <div className="first-row">
-                    <button className="keyboard-button" onClick={handleClickBtn} data-letter="A">a</button>
-                    <button className="keyboard-button" onClick={handleClickBtn} data-letter="B">b</button>
-                    <button className="keyboard-button" onClick={handleClickBtn} data-letter="C">c</button>
-                    <button className="keyboard-button" onClick={handleClickBtn} data-letter="D">d</button>
-                    <button className="keyboard-button" onClick={handleClickBtn} data-letter="E">e</button>
-                    <button className="keyboard-button" onClick={handleClickBtn} data-letter="F">f</button>
-                    <button className="keyboard-button" onClick={handleClickBtn} data-letter="G">g</button>
-                    <button className="keyboard-button" onClick={handleClickBtn} data-letter="H">h</button>
-                    <button className="keyboard-button" onClick={handleClickBtn} data-letter="I">i</button>
-                    <button className="keyboard-button" onClick={handleClickBtn} data-letter="J">j</button>
+                    <button className="keyboard-button" data-letter="A">a</button>
+                    <button className="keyboard-button" data-letter="B">b</button>
+                    <button className="keyboard-button" data-letter="C">c</button>
+                    <button className="keyboard-button" data-letter="D">d</button>
+                    <button className="keyboard-button" data-letter="E">e</button>
+                    <button className="keyboard-button" data-letter="F">f</button>
+                    <button className="keyboard-button" data-letter="G">g</button>
+                    <button className="keyboard-button" data-letter="H">h</button>
+                    <button className="keyboard-button" data-letter="I">i</button>
+                    <button className="keyboard-button" data-letter="J">j</button>
                 </div>
                 <div className="second-row">
-                    <button className="keyboard-button" onClick={handleClickBtn} data-letter="K">k</button>
-                    <button className="keyboard-button" onClick={handleClickBtn} data-letter="L">l</button>
-                    <button className="keyboard-button" onClick={handleClickBtn} data-letter="M">m</button>
-                    <button className="keyboard-button" onClick={handleClickBtn} data-letter="N">n</button>
-                    <button className="keyboard-button" onClick={handleClickBtn} data-letter="O">o</button>
-                    <button className="keyboard-button" onClick={handleClickBtn} data-letter="P">p</button>
-                    <button className="keyboard-button" onClick={handleClickBtn} data-letter="Q">q</button>
-                    <button className="keyboard-button" onClick={handleClickBtn} data-letter="R">r</button>
-                    <button className="keyboard-button" onClick={handleClickBtn} data-letter="S">s</button>
-                    <button className="keyboard-button" onClick={handleClickBtn} data-letter="T">t</button>
+                    <button className="keyboard-button" data-letter="K">k</button>
+                    <button className="keyboard-button" data-letter="L">l</button>
+                    <button className="keyboard-button" data-letter="M">m</button>
+                    <button className="keyboard-button" data-letter="N">n</button>
+                    <button className="keyboard-button" data-letter="O">o</button>
+                    <button className="keyboard-button" data-letter="P">p</button>
+                    <button className="keyboard-button" data-letter="Q">q</button>
+                    <button className="keyboard-button" data-letter="R">r</button>
+                    <button className="keyboard-button" data-letter="S">s</button>
+                    <button className="keyboard-button" data-letter="T">t</button>
                 </div>
                 <div className="third-row">
-                    <button className="keyboard-button" onClick={handleClickBtn} data-letter="U">u</button>
-                    <button className="keyboard-button" onClick={handleClickBtn} data-letter="V">v</button>
-                    <button className="keyboard-button" onClick={handleClickBtn} data-letter="W">w</button>
-                    <button className="keyboard-button" onClick={handleClickBtn} data-letter="X">x</button>
-                    <button className="keyboard-button" onClick={handleClickBtn} data-letter="Y">y</button>
-                    <button className="keyboard-button" onClick={handleClickBtn} data-letter="Z">z</button>
-                    <button className="keyboard-button" onClick={handleClickBtn} data-letter="-">-</button>
+                    <button className="keyboard-button" data-letter="U">u</button>
+                    <button className="keyboard-button" data-letter="V">v</button>
+                    <button className="keyboard-button" data-letter="W">w</button>
+                    <button className="keyboard-button" data-letter="X">x</button>
+                    <button className="keyboard-button" data-letter="Y">y</button>
+                    <button className="keyboard-button" data-letter="Z">z</button>
+                    <button className="keyboard-button" data-letter="-">-</button>
                 </div>
             </div>
         </>
